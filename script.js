4ab0f1189c224913728b6e01db3cb12aab4ca4dd
pedroairees <pedrocoelho2209@gmail.com>
2025-09-16 14:31:45 -0300

    finished project

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ const form = document.querySelector('form')
 const newItem = document.getElementById('addItem')
 const itemList = document.querySelector('.itens-list')
 
+const alertNewItem = document.querySelector('.alert-add')
+const alertDelItem = document.querySelector('.alert-remove')
+
 form.onsubmit = (event) => {
     event.preventDefault()
     creatingNewItem()
+    
 }
 
-
 function creatingNewItem() {
+    let nameItemList = newItem.value.replaceAll(" ", "-")
+
     const divItem = document.createElement('div')
-    divItem.classList.add('item')
+    divItem.classList.add('item', nameItemList)
     itemList.prepend(divItem)
     
     const divItemName = document.createElement('div')
@@ -19,13 +24,12 @@ function creatingNewItem() {
 
     const inputCheckbox = document.createElement('input')
     inputCheckbox.type = 'checkbox'
-    let nameItemList = newItem.value.replace(" ", "-")
     inputCheckbox.id = nameItemList
     inputCheckbox.name = nameItemList
     divItemName.prepend(inputCheckbox)
 
     const labelItem = document.createElement('label')
-    labelItem.for = nameItemList
+    labelItem.setAttribute('for', nameItemList)
     labelItem.textContent = newItem.value
     divItemName.append(labelItem)
 
@@ -33,5 +37,33 @@ function creatingNewItem() {
     btnDel.classList.add('btn-dell-item')
     btnDel.innerHTML = '<img src="./images/bin.svg" alt="Imagem ilustrativa de um lixeira">'
     divItem.append(btnDel)
+    btnDel.addEventListener('click', () => {
+        itemList.removeChild(divItem)
+    })
+
+    alertNewItem.classList.remove('display-none')
+
+    newItem.value = ''
+    newItem.focus()
 }
 
+const btnRemoveAlertRemove = document.getElementById('remove-alert-remove')
+itemList.addEventListener('click', (event) => {
+    const btnDel = event.target.closest('.btn-dell-item')
+
+    if(btnDel) {
+        const itemDelete = btnDel.closest('.item')
+        itemDelete.remove()
+        alertDelItem.classList.remove('display-none')
+    }
+})
+
+const btnRemoveAlertAddition = document.getElementById('remove-alert-addition')
+btnRemoveAlertAddition.addEventListener('click', () => {
+    alertNewItem.classList.add('display-none')
+})
+
+const btnRemoveAletDel = document.getElementById('remove-alert-remove')
+btnRemoveAlertRemove.addEventListener('click', () => {
+    alertDelItem.classList.add('display-none')
+})
